b410119e515d36680a4583df2fe48aa51ed86342
Trinity Chung <trinity@TrinityiMac.local>
2019-06-01 18:20:01 -0700

    3

---
+++ You Are Tank/Assets/Scripts/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public TankPart heart;
+    public int speed;
+    public List<List<TankPart>> parts;
+
+    private Rigidbody2D rb;
+    private Transform player;
+    private float rotationVelocity;
+    private float moveVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        player = FindObjectOfType<PlayerController>().transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(player);
+        rb.AddRelativeForce(Vector2.up * speed);
+    }
+
+}
--- You Are Tank/Assets/Scripts/PlayerController.cs
+++ You Are Tank/Assets/Scripts/PlayerController.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float unit;
-
     public TankPart heart;
     public int speed;
     public List<List<TankPart>> parts;
@@ -32,5 +30,10 @@ public class PlayerController : MonoBehaviour
         //rb.AddForce(new Vector2(Mathf.Cos(rb.rotation) * moveVelocity, Mathf.Sin(rb.rotation) * moveVelocity));
         rb.AddRelativeForce(Vector2.up * moveVelocity);
         rb.angularVelocity += rotationVelocity;
+    }
+
+    public void AddPart()
+    {
+
     }
 }
